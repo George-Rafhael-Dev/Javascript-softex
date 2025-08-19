9fafb649381b6eb05be567a767b8d845b7f33965
G. Rafhael A. Silva <georgerafhael113@gmail.com>
2025-08-19 15:05:08 -0300

    aula 5

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let nome="Ivo"
-console.log("seu nome é "+nome);
-aleret
+let readline = require("readline-sync")
+let nm = readline.question("digite seu seu nome: ")
+let idd = readline.question("digite sua idade: ")
+if(idd <=0){
+    console.log ('idade inválida')
+}
+else if(idd <= 17){
+    console.log("Você é menor de idade, ", nm)
+}
+else{
+    console.log(`Você é maior de idade, ${nm}`)
+}
