cab223c94bc1789787d97245f1613b91970c37a4
G. Rafhael A. Silva <georgerafhael113@gmail.com>
2025-09-02 16:22:09 -0300

    k

---
+++ aula7/aula7.ts
@@ -0,0 +1,16 @@
+class Carro {
+    private marca : String
+    public cor : String
+    public placa : String
+    public modelo : String
+    public ano : Number
+    
+    ligar(){
+
+    }
+    desligar(){
+
+    }
+}
+
+const carro1 = new Carro()
